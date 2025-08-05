f0740b0994c57237b2e87f169c4bd98f9144ff39
Vndrvs <99025350+Vndrvs@users.noreply.github.com>
2025-08-06 02:38:41 +0700

    gamestate introduced

--- NAO-115/src/game/state.hpp
+++ NAO-115/src/game/state.hpp
@@ -1,13 +1,18 @@
-//
-//  state.hpp
-//  NAO-115
-//
-//  Created by 未来 ガジェット on 31/07/2025.
-//
+#pragma once
 
-#ifndef state_hpp
-#define state_hpp
+#include <string>
 
-#include <stdio.h>
-
-#endif /* state_hpp */
+struct GameState {
+    int pot; // total chips in the pot
+    int playerChips[2]; // total chips remaining per players (we also call this stack)
+    int playerBets[2]; // current street chip bets per players
+    int currentPlayer; // 0: Big Blind, 1: Small Blind
+    int street; // current street, updated when we see a '/' in the actionstring
+                // 0: preflop, 1: flop, 2: turn, 3: river
+    bool isTerminal; // I'll try to explain this. 'isTerminal' is a custom variable which signals
+                     // that we don't expect any player action after the last action string we receive or send
+                     // eg. Fold -> someone folded, the games immediately
+                     // All in bet and a player calls on it -> this can happen at any street, no further actions can be taken
+                     // Check - Check on River -> (onon-all-in terminal situation, game simply ends)
+    std::string actionHistory; // full action history from both players, matching Slumbot's formatting
+};
