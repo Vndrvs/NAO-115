4e98428c69b9d91e8966437711e23d48e09b9e6c
Vndrvs <99025350+Vndrvs@users.noreply.github.com>
2025-08-05 20:59:14 +0700

    bro look at the amount of comments

--- NAO-115/src/game/action.cpp
+++ NAO-115/src/game/action.cpp
@@ -1,8 +1,74 @@
-//
-//  action.cpp
-//  NAO-115
-//
-//  Created by 未来 ガジェット on 31/07/2025.
-//
-
 #include "action.hpp"
+
+namespace Game {
+    // this function gets the action character as a parameter and assigns an enum 'ActionType' for the corresponding letter
+    std::optional<Action> getAction(const std::string& s) {
+        if (s.empty()) return std::nullopt;
+        
+        // we always check the first character of the string, this might seem useless at first sight
+        // because fold, call and check only come as one char, but bet / raise action always comes with bet sizing
+        // so in those cases we need to tell Nao that the action is always the first character of the input string
+        char c = s[0];
+        
+        // characters exactly match the API response received from Slumbot, quite straightforward
+        // in the struct 'ActionType' created in the header we have type (enummed beforehand) and amount (int)
+        // but we only use the amount variable if the action is a bet or a raise, otherwise it's always 0
+        if (c == 'f') return Action{ActionType::Fold, 0};
+        if (c == 'c') return Action{ActionType::Call, 0};
+        // note: as 'c' is already taken for 'call', Slumbot uses 'k' for check, our enum is also set this way
+        if (c == 'k') return Action{ActionType::Check, 0};
+        // 'b' can mean bet or raise depending on the context,
+        if (c == 'b') {
+            try {
+                // we get the substring after the first character which defines the amount on bet / raise
+                // with stoi we convert it to an int, then we return the corresponding action
+                int amount = std::stoi(s.substr(1));
+                return Action{ActionType::Bet, amount};
+            // catch ... means if we catch anything
+            } catch (...) {
+                return std::nullopt;
+            }
+        }
+
+        return std::nullopt;
+    }
+
+    uint32_t encodeAction(const Action& a) {
+        // first 2 bits action type, following 30 bits bet/raise amount
+        // type refers back to the enum created in hpp (0:fold, 1:call, 2:check, 3:bet)
+        
+        uint32_t bits = static_cast<uint32_t>(a.type) & 0b11;
+
+        if (a.type == ActionType::Bet) {
+            uint32_t amt = static_cast<uint32_t>(a.amount);
+            bits |= (amt << 2); // bitshift the amount to start from 3rd bit
+        }
+
+        return bits;
+    }
+
+    // thought about creating a char based constexpr for the actions, but switch seems faster, will benchmark later to decide.
+    std::string readAction(uint32_t bits) {
+        // our of the 32 available bits, we used the first 2 to determine our action type
+        // so first we mask the first two, than throw in into the static cast machine (lol) which will pass the enum type action
+        ActionType type = static_cast<ActionType>(bits & 0b11);
+        
+        // depending on the received enum, we throw the corresponding character
+        switch (type) {
+            // again, these are straightforward
+            case ActionType::Fold: return "f";
+            case ActionType::Call: return "c";
+            case ActionType::Check: return "k";
+            // in case of bet,
+            case ActionType::Bet: {
+                // we shift right by two bits, so only the bits corresponding to the bet amount remain
+                // and we cast the bit type number to an integer to get the actual decimal (normal number) representation of the bet size
+                int amount = static_cast<int>(bits >> 2);
+                // and after adding 'b', we convert the
+                return "b" + std::to_string(amount);
+            }
+            default: return "?";
+        }
+    }
+
+}
--- NAO-115/src/game/action.hpp
+++ NAO-115/src/game/action.hpp
@@ -1,13 +1,33 @@
-//
-//  action.hpp
-//  NAO-115
-//
-//  Created by 未来 ガジェット on 31/07/2025.
-//
+#pragma once
 
-#ifndef action_hpp
-#define action_hpp
+#include <string>
+#include <optional>
+#include <cstdint>
 
-#include <stdio.h>
+namespace Game {
 
-#endif /* action_hpp */
+    enum class ActionType : uint8_t {
+        Fold,
+        Call,
+        Check,
+        Bet
+    };
+
+    struct Action {
+        ActionType type;
+        int amount; // only exist if action is bet/raise
+    };
+
+    // from string to action ('f, c, k, b(n)
+    std::optional<Action> getAction(const std::string&);
+
+    // action to bits
+    // why only 32 bits? well the first 2 will tell us the action type
+    // and the remaining 30 will be enough to cover the total stack size of Slumbot which is 20,000 in total
+    // well if I eventually get an even stronger opponent, I'll optimize it further for that scenario, but it's OK now
+    uint32_t encodeAction(const Action&);
+
+    // decodes bits to readable strings again
+    std::string readAction(uint32_t);
+
+}
