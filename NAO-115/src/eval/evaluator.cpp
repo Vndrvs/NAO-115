59cb03bc615817adf27c838e9f47ac85f2be8579
Vndrvs <99025350+Vndrvs@users.noreply.github.com>
2026-01-03 02:06:04 +0700

    added 7 card eval

--- NAO-115/src/eval/evaluator.cpp
+++ NAO-115/src/eval/evaluator.cpp
@@ -86,4 +86,107 @@ inline int eval_5(int c1, int c2, int c3, int c4, int c5) {
     return 7463 - hashRanks[find_fast(product)];
 }
 
+inline int get_straight_flush_rank(int mask) {
+
+    int tmp = mask & (mask << 1) & (mask << 2) & (mask << 3) & (mask << 4);
+    
+    if (tmp != 0) {
+        int bit = 31 - __builtin_clz(tmp);
+        return bit;
+    }
+    
+    if ((mask & 0x100F) == 0x100F) return 3;
+    
+    return -1;
+}
+
+int evaluate7(const std::vector<int>& cards) {
+    int h[7];
+    for(int i=0; i<cards.size(); ++i) h[i] = deck[cards[i]];
+    
+    int suit_counts[4] = {0};
+    int suit_masks[4] = {0};
+    int full_rank_mask = 0;
+    
+    for(int i=0; i<cards.size(); ++i) {
+        int rank_bit = (h[i] >> 16);
+        full_rank_mask |= rank_bit;
+        
+        if (h[i] & 0x1000) { suit_counts[0]++; suit_masks[0] |= rank_bit; }
+        else if (h[i] & 0x2000) { suit_counts[1]++; suit_masks[1] |= rank_bit; }
+        else if (h[i] & 0x4000) { suit_counts[2]++; suit_masks[2] |= rank_bit; }
+        else { suit_counts[3]++; suit_masks[3] |= rank_bit; }
+    }
+    
+    for(int i=0; i<4; i++) {
+        if (suit_counts[i] >= 5) {
+            int mask = suit_masks[i];
+            
+            int sf_top = get_straight_flush_rank(mask);
+            if (sf_top != -1) {
+
+                int sf_mask = 0;
+                if (sf_top == 3) sf_mask = 0x100F;
+                else sf_mask = (0x1F << (sf_top - 4));
+                
+                return 7463 - flushRanks[sf_mask];
+            }
+            
+            int final_mask = 0;
+            int count = 0;
+            for(int r=12; r>=0; r--) {
+                if ((mask >> r) & 1) {
+                    final_mask |= (1 << r);
+                    count++;
+                    if (count == 5) break;
+                }
+            }
+            return 7463 - flushRanks[final_mask];
+        }
+    }
+    
+    if (__builtin_popcount(full_rank_mask) == cards.size()) {
+        int mask = 0;
+        int bits_found = 0;
+        for (int r=12; r>=0; r--) {
+            if ((full_rank_mask >> r) & 1) {
+                mask |= (1 << r);
+                bits_found++;
+                if (bits_found == 5) break;
+            }
+        }
+        return 7463 - uniqueRanks[mask];
+    }
+    
+    int max_score = 0;
+    for (int i = 0; i < 21; i++) {
+        int score = eval_5(
+            h[permutations[i][0]], h[permutations[i][1]], h[permutations[i][2]],
+            h[permutations[i][3]], h[permutations[i][4]]
+        );
+        if (score > max_score) max_score = score;
+    }
+    
+    return max_score;
+}
+
+int parseCard(const std::string& cardStr) {
+    if (cardStr.size() < 2) return -1;
+    char r = cardStr[0];
+    char s = cardStr[1];
+    int ri = 0;
+    if (isdigit(r)) ri = r - '2';
+    else if (r == 'T') ri = 8;
+    else if (r == 'J') ri = 9;
+    else if (r == 'Q') ri = 10;
+    else if (r == 'K') ri = 11;
+    else if (r == 'A') ri = 12;
+    int si = 0;
+    if (s == 'c') si = 0;
+    else if (s == 'd') si = 1;
+    else if (s == 'h') si = 2;
+    else if (s == 's') si = 3;
+    return ri * 4 + si;
+}
+
 }
