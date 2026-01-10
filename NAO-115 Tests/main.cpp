2451d7cb8b91603447be30d68f4fa0a5861fab45
Vndrvs <99025350+Vndrvs@users.noreply.github.com>
2026-01-10 15:15:06 +0700

    safety commit

--- NAO-115 Tests/main.cpp
+++ NAO-115 Tests/main.cpp
@@ -1,14 +1,1 @@
-// NAO-115 Tests/main.cpp
-#include <iostream>
 
-void run_comparison_test();
-
-int main(int argc, const char * argv[]) {
-    std::cout << "=== NAO-115 TEST SUITE ===\n";
-    
-    std::cout << "\n";
-    
-    run_comparison_test();
-    
-    return 0;
-}
