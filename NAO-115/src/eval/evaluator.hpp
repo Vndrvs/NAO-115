83e7f66480ca1a4636f9b6bb7447f111c9265471
Vndrvs <99025350+Vndrvs@users.noreply.github.com>
2026-02-12 15:13:25 +0800

    abstraction arithmetic done, working on bucketer

--- NAO-115/src/eval/evaluator.hpp
+++ NAO-115/src/eval/evaluator.hpp
@@ -11,11 +11,12 @@ extern int deck[52];
 
 void initialize();
 inline int eval_5(int c1, int c2, int c3, int c4, int c5);
+int eval_6(int c1, int c2, int c3, int c4, int c5, int c6);
+int eval_7(int c1, int c2, int c3, int c4, int c5, int c6, int c7);
 int evaluate5(const std::vector<int>& cards);
 int evaluate6(const std::vector<int>& cards);
 int evaluate7(const std::vector<int>& cards);
 int parseCard(const std::string& cardStr);
-int eval_7(const int* cards);
 
 }
 
